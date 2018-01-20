a2cd9328a8c8f48beef31f4a27e507fb70b15dc2
Siva Dhayalan <siva@dhayalan.com>
2018-01-20 00:50:11 -0500

    removed old and unwanted files

--- project/script.php
+++
@@ -1,7 +0,0 @@
-<?php
-$fh=fopen("users.txt", 'a') or die("cant open file!");
-$username=$_POST['username'];
-$password=$_POST['password'];
-echo fwrite($fh,$username . ':' . $password);
-fclose($fh);
-?>
