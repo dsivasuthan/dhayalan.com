e66106d681dd0353175ce34233e274a1d01667b1
Siva Dhayalan <dsivasuthan@gmail.com>
2019-07-09 00:03:26 -0400

    trying the json importer

---
+++ vuejs-with-import-json-example/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div>
+    <ul>
+    <li v-bind:key="name" v-for="name in names">
+      {{ name }}
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import states from '../assets/data.json';
+
+export default {
+  name: 'HelloWorld',
+  computed: {
+    names() {
+      return states.accounts.map((item) => {
+        return item.name;
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
